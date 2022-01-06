e23572b01b5d52caa4498ec2bb7055964215b4ff
n3k0m4 <achraf.el-masdouri@imt-atlantique.net>
2022-01-06 20:50:59 +0100

    Adding working movements

--- include/utils.h
+++ include/utils.h
@@ -2,41 +2,8 @@
 #define UTILS_H
 
 #include <stdio.h>
-#include "ev3.h"
-#include "ev3_port.h"
-#include "ev3_tacho.h"
 #include <string.h>
-#define leftMotor "outB"
-#define rightMotor "outA"
-static uint8_t i;
 
-static uint8_t getLeftMotor(){
-    if (ev3_tacho_init() < 1)
-        printf("TACHO NOT INITIATED !");
+#define Sleep(msec) usleep((msec)*1000
 
-    for (i = 0; i < DESC_LIMIT; i++)
-    {
-        if (ev3_tacho[i].type_inx != TACHO_TYPE__NONE_){
-            if(strcmp(leftMotor, ev3_tacho_type(ev3_tacho[i].type_inx))==0)
-        {
-            printf("  index is = %d\n", i);
-            return i;
-        }
-    }}
-}
-static uint8_t getRighttMotor(){
-    
-    if (ev3_tacho_init() < 1)
-        printf("TACHO NOT INITIATED !");
-
-    for (i = 0; i < DESC_LIMIT; i++)
-    {
-        if (ev3_tacho[i].type_inx != TACHO_TYPE__NONE_){
-            if (strcmp(rightMotor, ev3_tacho_type(ev3_tacho[i].type_inx))==0)
-        {
-            printf("  index is = %d\n", i);
-            return i;
-        }
-    }
-}}
 #endif
--- source/movement.c
+++ source/movement.c
@@ -1,19 +1,88 @@
-#include <movement.h>
-#include <utils.h>
+#include "movement.h"
+uint8_t m_left;
+uint8_t m_right;
+int max_speed;
+int max_speed_left;
+int max_speed_right;
 
-static void move(uint8_t speed, float distance){
-    uint8_t leftMotor = getLeftMotor();
-    uint8_t rightMotor = getRightMotor();
-    set_tacho_speed_sp(leftMotor, 1500* 2 / 3);
-    set_tacho_speed_sp(rightMotor, 1500* 2 / 3);
-    Sleep(10);    
-    exit(0);
+// Init the motors and set max_speed
+void init() {
+    while (ev3_tacho_init() < 1)
+        sleep(1000);
+    ev3_search_tacho_plugged_in(port_left, 0, &m_left, 0);
+    ev3_search_tacho_plugged_in(port_right, 0, &m_right, 0);
+    get_tacho_max_speed(m_left, &max_speed_left);
+    get_tacho_max_speed(m_right, &max_speed_right);
+    max_speed = max_speed_right > max_speed_left ? max_speed_left : max_speed_right;
+}
+// Work done, Stop motors
+void finished() {
+    set_tacho_stop_action_inx(m_left, TACHO_COAST);
+    set_tacho_stop_action_inx(m_right, TACHO_COAST);
+    set_tacho_command_inx(m_left, TACHO_STOP);
+    set_tacho_command_inx(m_right, TACHO_STOP);
 }
 
-static void turnLeft(float angle){
-
+// Zoom forward with a speed
+int moveForward(int speed) {
+    // speed 0 just to able to launch with max_speed without knowing it
+    if (speed == 0 || speed > max_speed)
+        speed = max_speed;
+    set_tacho_speed_sp(m_left, speed);
+    set_tacho_speed_sp(m_right, speed);
+    set_tacho_command_inx(m_left, TACHO_RUN_FOREVER);
+    set_tacho_command_inx(m_right, TACHO_RUN_FOREVER);
+    return 0;
 }
 
-static void turnRight(float angle){
-    
+// Move only the left motor
+int turnLeftMotor(int speed) {
+    if (speed == 0 || speed > max_speed)
+        speed = max_speed;
+    set_tacho_speed_sp(m_left, speed);
+    set_tacho_stop_action_inx(m_right, TACHO_COAST);
+    set_tacho_command_inx(m_right, TACHO_STOP);
+    set_tacho_command_inx(m_left, TACHO_RUN_FOREVER);
+    return 0;
+}
+
+// Move only the right motor
+int turnRightMotor(int speed) {
+    if (speed == 0 || speed > max_speed)
+        speed = max_speed;
+    set_tacho_speed_sp(m_left, speed);
+    set_tacho_stop_action_inx(m_left, TACHO_COAST);
+    set_tacho_command_inx(m_left, TACHO_STOP);
+    set_tacho_command_inx(m_right, TACHO_RUN_FOREVER);
+    return 0;
+}
+
+// Move backward
+int moveBackward(int speed) {
+    if (speed == 0 || speed > max_speed)
+        speed = max_speed;
+    speed = -speed;
+    set_tacho_speed_sp(m_left, speed);
+    set_tacho_speed_sp(m_right, speed);
+    set_tacho_command_inx(m_left, TACHO_RUN_FOREVER);
+    set_tacho_command_inx(m_right, TACHO_RUN_FOREVER);
+    return 0;
+}
+
+// Stop with hard brakes, effective to halt and recover 
+int stop() {
+    set_tacho_stop_action_inx(m_left, TACHO_HOLD);
+    set_tacho_stop_action_inx(m_right, TACHO_HOLD);
+    set_tacho_command_inx(m_left, TACHO_STOP);
+    set_tacho_command_inx(m_right, TACHO_STOP);
+    return 0;
+}
+
+// Stop with soft brakes
+int softStop(){
+    set_tacho_stop_action_inx(m_left, TACHO_BRAKE);
+    set_tacho_stop_action_inx(m_right, TACHO_BRAKE);
+    set_tacho_command_inx(m_left, TACHO_STOP);
+    set_tacho_command_inx(m_right, TACHO_STOP);
+    return 0;
 }
