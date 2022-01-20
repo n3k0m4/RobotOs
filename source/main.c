1a1ecfb0513ba6a6931600ad85e70405415f17f9
Farouk <faiz@eurecom.fr>
2022-01-20 11:37:46 +0100

    Stop robot on SIGINT

--- source/main.c
+++ source/main.c
@@ -2,11 +2,20 @@
 #include <stdlib.h>
 #include "utils.h"
 #include "strats.h"
+#include <signal.h>
+
+void stop_handler(){
+    stop(TACHO_COAST);
+    exit(0);
+}
 
 int main(void)
 {
     // always_left(start_angle);
     setvbuf(stdout, NULL, _IONBF, 0);
+    struct sigaction sa;
+    sa.sa_handler = stop_handler;
+    sigaction (SIGINT, &sa, NULL);
     int gyro_value;
     int last_gyro_value;
     printf("*** ( EV3 ) Hello! ***\n");
