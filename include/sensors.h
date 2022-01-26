3b3526e68cc4cfdb3310550925fcf46ebc224932
n3k0m4 <achraf.el-masdouri@imt-atlantique.net>
2022-01-26 15:46:25 +0100

    Doc for sensors.h

--- include/sensors.h
+++ include/sensors.h
@@ -6,18 +6,57 @@
 #include "ev3.h"
 #include "ev3_sensor.h"
 
-#define ACCIDENT_THRESHOLD 15
-#define INLINE_THRESHOLD 10
-#define GYRO_CAL_MODE "GYRO-CAL"
-#define GYRO_ANG_MODE "GYRO-ANG"
+#define ACCIDENT_THRESHOLD 15    ///< Angular difference threshold to detect accident
+#define INLINE_THRESHOLD 10      ///< Agular differece to keep the robot's movement inline
+#define GYRO_CAL_MODE "GYRO-CAL" ///< Gyroscope's calibration mode
+#define GYRO_ANG_MODE "GYRO-ANG" ///< Gyroscope's angular mode
 
+/**
+ * Detects the sensors and sets their serial numbers. 
+ * 
+ * The sensors initiated are Gyroscope, sonar, and Touch sensor 
+ */
 void init_sensors();
+
+/**
+ * Get gyroscope value in degrees.
+ * 
+ * @param[out] value_buf Buffer for result
+ */
 void get_gyro_value(int *value_buf);
+
+/**
+ * Get distance from sonar in cm
+ * 
+ * @param[out] value_buf Buffer for result
+ */
 void get_sonar_value(int *value_buf);
-int get_stable_sonar_value(int *value_buf);
-void get_color_value(int *value_buf);
-void get_color_value2(int *value_buf1, int *value_buf2, int *value_buf3);
+
+/**
+ * Get distance stable from sonar. This method stops the robot and compare consecutive values with a threshold
+ * 
+ * @param[out] value_buf Buffer for result
+ */
+void get_stable_sonar_value(int *value_buf);
+
+/**
+ * Checks if the touch sensor is pushed
+ * 
+ * @return boolean valus depending on the state of the sensor 
+ */
 bool check_pressed();
+
+/**
+ * Checks if the touch sensor is pushed
+ * @param[in] previous_angle The original angle of the robot before the accident
+ * @param[in] current_angle The current angle of the robot before the accident
+ * @return boolean valus depending on the state of the sensor 
+ */
 bool detect_accident(int previous_angle, int current_angle);
+
+/**
+ * Util function to calibrate the gyroscope by swiping to GYRO_CAL_MODE and back to GYRO_ANG_MODE 
+ * 
+ */
 void calibrate_gyro();
 #endif
--- source/sensors.c
+++ source/sensors.c
@@ -51,24 +51,29 @@ bool detect_accident(int previous_angle, int current_angle)
     return false;
 }
 
-int get_stable_sonar_value(int *value_buf){
+void get_stable_sonar_value(int *value_buf)
+{
     int previous_sonar_value;
     get_sonar_value(&previous_sonar_value);
     const int ERROR_THRESHOLD = 20; // 2cm
     int sonar_value;
     int nb_constant_measures = 0;
-    while (nb_constant_measures < 2){
+    while (nb_constant_measures < 2)
+    {
         SLEEP(500);
         get_sonar_value(&sonar_value);
         printf("Getting stable sonar value ...\n");
-        if (abs(previous_sonar_value - sonar_value) < ERROR_THRESHOLD) nb_constant_measures++;
-        else nb_constant_measures = 0;
+        if (abs(previous_sonar_value - sonar_value) < ERROR_THRESHOLD)
+            nb_constant_measures++;
+        else
+            nb_constant_measures = 0;
         previous_sonar_value = sonar_value;
     }
     *value_buf = sonar_value;
 }
 
-void calibrate_gyro(){
+void calibrate_gyro()
+{
     printf("Calibrating gyro ...\n");
     set_sensor_mode(sn_gyro, GYRO_CAL_MODE);
     SLEEP(1000);
